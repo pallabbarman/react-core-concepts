718fe7bd12d1148c6c09883c7f5ceddac23d6e57
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-23 17:26:09 +0600

    pass dynamic data

--- src/App.js
+++ src/App.js
@@ -6,24 +6,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person name="Tom Cruise" profession="Actor"></Person>
+        <Person name="Kate" profession="Actress"></Person>
       </header>
     </div>
   );
 }
-function Person() {
+
+function Person(props) {
   const personStyle = {
-    border: '2px solid green',
-    margin: '10px',
-    borderRadius: '10px'
+    border:'2px solid green',
+    margin: '20px',
+    borderRadius: '10px',
+    width:'500px'
   }
   return (
     <div style={personStyle}>
-      <h1>Name: Tom khan</h1>
-      <h3>Hero of the year</h3>
+      <h1>Name: {props.name}</h1>
+      <h3>Profession: {props.profession}</h3>
     </div>
   )
 }
