b7d86f186a28f79749dd567f641873b69eecedbf
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-23 17:50:51 +0600

    Pass object to components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    { name: 'PhotoShop', price: '$90.99' },
+    { name: 'Illustrator', price: '$60.99' },
+    { name: 'PDF Reader', price: '$6.99' }
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
         <Person name="Tom Cruise" profession="Actor"></Person>
         <Person name="Kate" profession="Actress"></Person>
       </header>
@@ -13,17 +20,37 @@ function App() {
   );
 }
 
+function Product(props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '10px',
+    backgroundColor: 'darkgray',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+  const { name, price } = props.product;
+  console.log(name, price);
+  return (
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+
 function Person(props) {
   const personStyle = {
-    border:'2px solid green',
+    border: '2px solid green',
     margin: '20px',
     borderRadius: '10px',
-    width:'500px'
+    width: '500px'
   }
   return (
     <div style={personStyle}>
       <h1>Name: {props.name}</h1>
-      <h3>Profession: {props.profession}</h3>
+      <h2>Profession: {props.profession}</h2>
     </div>
   )
 }
